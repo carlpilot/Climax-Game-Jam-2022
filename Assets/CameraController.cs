e247ca75c81e52cce7fb80182aef5d9c55e2ebc2
carlpilot <carlbert101@gmail.com>
2022-03-04 20:33:37 +0000

    Player controller updates

---
+++ Assets/CameraController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Objects")]
+    public Transform A;
+    public Transform B;
+
+    [Header("Parameters")]
+    public Vector3 cameraOffset;
+    public float cameraFollowSpeed;
+    public float cameraLookSpeed;
+    public float cameraZoomMult;
+    public float minZoom = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      var target = (A.position + B.position) / 2f;
+      var dist = (A.position - B.position).magnitude;
+      if (dist < minZoom) dist = minZoom;
+      var zoomMult = dist * cameraZoomMult;
+      var targetPosition = target + (cameraOffset*zoomMult);
+      transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * cameraFollowSpeed);
+      transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(target-transform.position), Time.deltaTime * cameraLookSpeed);
+    }
+}
