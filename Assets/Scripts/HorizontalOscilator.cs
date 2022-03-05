e33cb99a5c8539c141cd61fb0452d5d41a17e3b9
Josh Pattman <Josh.pattman@gmail.com>
2022-03-05 19:59:27 +0000

    button activated thingy

--- Assets/Scripts/HorizontalOscilator.cs
+++ Assets/Scripts/HorizontalOscilator.cs
@@ -8,6 +8,7 @@ public class HorizontalOscilator : MonoBehaviour
   Vector3 startPos;
   float dir = 1;
   public float speed = 1f;
+  public PushButton button;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +18,13 @@ public class HorizontalOscilator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (transform.position.x > startPos.x + maxX){
-        dir = -1;
-      } else if (transform.position.x < startPos.x){
-        dir = 1;
+      if (button.isPushed){
+        if (transform.position.x > startPos.x + maxX){
+          dir = -1;
+        } else if (transform.position.x < startPos.x){
+          dir = 1;
+        }
+        transform.Translate(new Vector3(dir*speed*Time.deltaTime, 0, 0));
       }
-      transform.Translate(new Vector3(dir*speed*Time.deltaTime, 0, 0));
     }
 }
