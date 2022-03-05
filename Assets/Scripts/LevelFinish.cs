f13c6dda158dec8675f1e8cd1d31dfc9ed192832
carlpilot <carlbert101@gmail.com>
2022-03-05 15:17:33 +0000

    Finish level 1

--- Assets/Scripts/LevelFinish.cs
+++ Assets/Scripts/LevelFinish.cs
@@ -2,26 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelFinish : MonoBehaviour
-{
-  public Animator anim;
-  public PushButton a;
-  public PushButton b;
-    // Start is called before the first frame update
-    void Start()
-    {
+public class LevelFinish : MonoBehaviour {
+    public Animator anim;
+    public PushButton a;
+    public PushButton b;
+
+    void Start () {
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-      if (a.isPushed && b.isPushed){
-        Finish();
-      }
+    void Update () {
+        if (a.isPushed && b.isPushed) {
+            Finish ();
+        }
     }
 
-    void Finish(){
-      anim.SetBool("doorOpen", true);
+    void Finish () {
+        anim.SetBool ("doorOpen", true);
+        FindObjectOfType<GameManager> ().Win ();
     }
 }
