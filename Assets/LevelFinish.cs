4d4aabe622c577e13ef9889a0bd4dbd81986cd12
Josh Pattman <Josh.pattman@gmail.com>
2022-03-05 11:58:58 +0000

    door works

--- Assets/LevelFinish.cs
+++ Assets/LevelFinish.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class LevelFinish : MonoBehaviour
 {
+  public Animator anim;
+  public PushButton a;
+  public PushButton b;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      if (a.isPushed && b.isPushed){
+        Finish();
+      }
+    }
+
+    void Finish(){
+      anim.SetBool("doorOpen", true);
     }
 }
