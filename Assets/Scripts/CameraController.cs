0e0de0b964a0dc111640a396db247b7b28005938
Josh Pattman <Josh.pattman@gmail.com>
2022-03-06 16:03:35 +0000

    Broken

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -15,6 +15,9 @@ public class CameraController : MonoBehaviour
     public float cameraZoomMult;
     public float minZoom = 3;
 
+    public float minX = 0f;
+    public float minHeight = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +32,8 @@ public class CameraController : MonoBehaviour
       if (dist < minZoom) dist = minZoom;
       var zoomMult = dist * cameraZoomMult;
       var targetPosition = target + (cameraOffset*zoomMult);
+      if (targetPosition.x < minX) targetPosition = new Vector3(minX, targetPosition.y, targetPosition.z);
+      //if (targetPosition.y < minHeight) targetPosition = new Vector3(targetPosition.x, minX, targetPosition.z);
       transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * cameraFollowSpeed);
       transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(target-transform.position), Time.deltaTime * cameraLookSpeed);
     }
