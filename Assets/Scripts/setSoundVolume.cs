db431009012194b2739d15bcdf047f4571c0bbaf
carlpilot <carlbert101@gmail.com>
2022-03-19 18:54:39 +0000

    Volume slider overhaul

--- Assets/Scripts/setSoundVolume.cs
+++ Assets/Scripts/setSoundVolume.cs
@@ -1,13 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
-public class setSoundVolume : MonoBehaviour
-{
+[RequireComponent (typeof (Slider))]
+public class setSoundVolume : MonoBehaviour {
+    public enum Mode { Sound, Music };
+    public Mode mode;
     public AudioMixer mixer;
-    public void SetLevel (float sliderValue)
-    {
-        mixer.SetFloat("SoundParam", Mathf.Log10(sliderValue) * 20);
+    public bool SaveToPrefs;
+    public string PlayerPrefName;
+    Slider s;
+
+    private void Start () {
+        s = GetComponent<Slider> ();
+        if (SaveToPrefs && PlayerPrefs.HasKey (PlayerPrefName)) s.value = PlayerPrefs.GetFloat (PlayerPrefName);
     }
+
+    public void SetLevel () {
+        mixer.SetFloat (GetName (), Mathf.Log10 (s.value) * 20);
+        if (SaveToPrefs) PlayerPrefs.SetFloat (PlayerPrefName, s.value);
+    }
+
+    public string GetName () => mode switch
+    {
+        Mode.Sound => "SoundParam",
+        Mode.Music => "MusicParam",
+        _ => throw new UnassignedReferenceException ("Sound enum")
+    };
 }
